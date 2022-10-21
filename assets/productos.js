a36f86d55996d9fed6fc9cc189d13ce1c1af9bfd
LuisReyes97 <genesis.dmop@gmail.com>
2022-10-21 16:03:54 -0500

    componente dinamico

--- assets/productos.js
+++ assets/productos.js
@@ -1,8 +1,5 @@
 export const productosDestacados = [
   {
-    id: 1,
-    titulo: "Los más vendidos",
-    resumen: "Estas son las últimas tendencias de nuestros clientes",
     code: ["+93","+358"],
     name: ["Afghanistan","Aland Islands"]
   },
