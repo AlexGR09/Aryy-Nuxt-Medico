177ed0becfdd7fb1718f5c2f1b7952b6c19fc700
LuisReyes97 <genesis.dmop@gmail.com>
2023-01-11 10:15:12 -0600

    dashboard

---
+++ components/dashboard/DashboardCitas.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-card class="rounded-xl card">
+    <v-row class="ma-0 pa-0">
+      <v-col md="">
+        <v-card-text class="text-no-wrap ">
+          CITA PRÓXIMA
+        </v-card-text>
+        <v-card-title class="text-no-wrap ">
+          Todavía no <br> tienes citas <br> agendadas
+        </v-card-title>
+        <v-card-subtitle class="d-flex align-center">
+        </v-card-subtitle>
+        <v-card-text class="d-flex align-center">
+        </v-card-text> 
+      </v-col>
+      <v-col md="4" cols="12">
+        <img
+          contain
+          height="110"
+          max-width="110"
+          class="greeting-card-trophy"
+          src="@/assets/icons/icon_consulta.svg"
+        >
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+
+    }
+  },
+
+};
+
+</script>
+
+<style lang="scss" scoped>
+.greeting-card {
+  position: relative;
+  .greeting-card-bg {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+  }
+  .greeting-card-trophy {
+    position: absolute;
+    bottom: 10%;
+    right: 10%;
+  }
+}
+// rtl
+.v-application {
+  &.v-application--is-rtl {
+    .greeting-card-bg {
+      right: initial;
+      left: 0;
+      transform: rotateY(180deg);
+    }
+    .greeting-card-trophy {
+      left: 2%;
+      right: initial;
+    }
+  }
+}
+
+.card :hover{
+  color: #ffffff;
+  background: #5d01db;
+}
+</style>
---
+++ components/dashboard/DashboardNoticias.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app">
+    <v-app id="inspire">
+      <v-card  max-width="800" class="rounded-xl">
+        <v-system-bar lights-out></v-system-bar>
+        <v-carousel
+          :continuous="false"
+          :cycle="cycle"
+          :show-arrows="false"
+          hide-delimiter-background
+          delimiter-icon="mdi-minus"
+          height="200"
+        >
+          <v-carousel-item v-for="(slide, i) in slides" :key="i">
+            <v-sheet :color="colors[i]" height="100%" tile>
+              <v-row class="fill-height" align="center" justify="center">
+                <div class="text-h2">{{ slide.title }}</div>
+                <v-img src="@/assets/icons/imagen.jpg"></v-img>
+              </v-row>
+            </v-sheet>
+          </v-carousel-item>
+        </v-carousel>
+
+        <v-table
+          class="roudend-ls mx-4 success--text text--darken-2"
+          elevation="8"
+        >
+        <v-card-actions>
+          <v-tab-transition>
+            <v-banner>
+              <th>{{slide.title}}</th>
+              <th>{{slide.subtitle}}</th>
+              <th>
+                <tr>
+                  
+                </tr>
+              </th>
+            </v-banner>
+          </v-tab-transition>
+        </v-card-actions>
+
+        </v-table>
+        <v-list two-line>
+          <v-list-item>
+            <v-list-item-avatar>
+              <v-img src="https://logos-download.com/wp-content/uploads/2016/10/Pfizer_logo.png"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>por <strong>Pfizer</strong></v-list-item-title>
+              <v-list-item-subtitle>en <strong>Congresos</strong></v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-app>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      colors: ['blue'],
+      slides: [
+        {
+          title: 'marketing',
+          subtitle: 'Todavía no has hecho ninguna campaña',
+          url: require('@/assets/icons/icon_cards_marketing.svg'),
+          description: 'Realiza tu primera campaña aqui',
+        },
+        {
+          title: 'sitio web',
+          subtitle: 'Todavía no has hecho ninguna campaña',
+          url: require('@/assets/icons/imagen.jpg'),
+          description: 'Realiza tu primera campaña aqui',
+        },
+        {
+          title:'diseño grafico',
+          subtitle: 'equipo de diseño graficoo en la vida',
+          descrition: 'Realiza tu primera campaña'
+        },
+        {
+          title: 'Redaccion',
+          subtitle: 'equipo de redaccion SooOooOo',
+          description:'Realiza tu primera campaña'
+        }
+      ],
+    }
+  },
+}
+</script>
