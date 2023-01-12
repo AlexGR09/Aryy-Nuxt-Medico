98b148d8e3cad84f77da8b1bf00a12e0c9c764da
LuisReyes97 <genesis.dmop@gmail.com>
2023-01-12 16:01:17 -0600

    ajustes

--- pages/settings/delete.vue
+++ pages/settings/delete.vue
@@ -1,8 +1,13 @@
 <template>
     <div>
-        <v-card>
-            <v-card-text>
-                holaaa delete
+        <v-card height="800px" flat class="pa-3 montserrat ">
+            <v-card-title>ELIMINAR CUENTA</v-card-title>
+            <v-card-subtitle>Al hacer click en continuar, estarás borrando permanentemente tu cuenta y todos los accesos
+                serán revocados. Perderás acceso a todos los beneficios que aryy te ofrece.
+                <br/> Este proceso es <b>permanente</b> y no se puede recuperar, deberás crear una cuenta nueva.
+            </v-card-subtitle>
+            <v-card-text class="justify-center" >
+                <h1 >¿Estás seguro que quieres eliminar tu cuenta?</h1>
             </v-card-text>
         </v-card>
     </div>
--- pages/settings/notifications.vue
+++ pages/settings/notifications.vue
@@ -1,14 +1,96 @@
 <template>
-    <div>
-        <v-card height="800px" flat class="pa-3 montserrat">
-            <v-card-title>
-                NOTIFICACIONES
-            </v-card-title>
-            <v-card-text>
-                <v-col>
-                    
-                </v-col>
-            </v-card-text>
-        </v-card>
-    </div>
+  <div>
+    <v-card height="800px" flat class="pa-3 montserrat">
+      <v-card-title> NOTIFICACIONES </v-card-title>
+      <v-card-text>
+        <v-row>
+          <v-col xl="4">
+            <h3>Citas nuevas</h3>
+            <h4>Notificaciones sobre las citas registradas en tu calendario</h4>
+          </v-col>
+          <v-col xl="3">
+            <v-switch
+              inset
+              v-model="pushRegis"
+              color="#9966ff"
+              label="Push"
+              hide-details
+            ></v-switch>
+            <v-switch
+              inset
+              v-model="emailRegis"
+              color="#9966ff"
+              label="Correo"
+              hide-details
+            ></v-switch>
+            <v-switch
+              inset
+              v-model="whatsRegis"
+              color="#9966ff"
+              label="Whatsapp"
+              hide-details
+            ></v-switch>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col xl="4">
+            <h3>Citas próximas</h3>
+            <h4>
+              Notificaciones sobre las citas cercanas a tu horario actual en tu
+              calendario
+            </h4>
+          </v-col>
+          <v-col xl="3">
+            <v-switch
+              inset
+              v-model="pushHor"
+              color="#9966ff"
+              label="Push"
+              hide-details
+            ></v-switch>
+            <v-switch
+              inset
+              v-model="emailHor"
+              color="#9966ff"
+              label="Correo"
+              hide-details
+            ></v-switch>
+            <v-switch
+              inset
+              v-model="whatsHor"
+              color="#9966ff"
+              label="Whatsapp"
+              hide-details
+            ></v-switch>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      pushRegis: false,
+      emailRegis: false,
+      whatsRegis: false,
+      pushHor: false,
+      emailHor: false,
+      whatsHor: false,
+    }
+  },
+}
+</script>
+<style scoped>
+h3 {
+  font-family: MontserratMedium;
+  color: #9966ff;
+}
+h4 {
+  font-family: Montserrat;
+  font-size: 1.8vh;
+  color: #999999;
+}
+</style>
